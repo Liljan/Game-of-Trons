94eff2132f3de9c79b7a7d21d656263bed595772
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-10-23 15:05:36 +0200

    Now it works :)

--- Game of Trons/Assets/Scripts/NetworkPlayerController.cs
+++ Game of Trons/Assets/Scripts/NetworkPlayerController.cs
@@ -19,28 +19,51 @@ public class NetworkPlayerController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        // go left
-        if (Input.GetAxis("Horizontal") < 0.0f && vx == 0.0f)
+
+        if (isLocalPlayer)
         {
-            vx = -speed;
-            vy = 0.0f;
-        }
-        else if (Input.GetAxis("Horizontal") > 0.0f && vx == 0.0f)
-        {
-            vx = speed;
-            vy = 0.0f;
-        }
-        else if (Input.GetAxis("Vertical") > 0.0f && vy == 0.0f)
-        {
-            vx = 0.0f;
-            vy = speed;
-        }
-        else if (Input.GetAxis("Vertical") < 0.0f && vy == 0.0f)
-        {
-            vx = 0.0f;
-            vy = -speed;
+            // go left
+            if (Input.GetAxis("Horizontal") < 0.0f && vx == 0.0f)
+            {
+                vx = -speed;
+                vy = 0.0f;
+
+                CmdMove(vx, vy);
+            }
+            else if (Input.GetAxis("Horizontal") > 0.0f && vx == 0.0f)
+            {
+                vx = speed;
+                vy = 0.0f;
+                CmdMove(vx, vy);
+            }
+            else if (Input.GetAxis("Vertical") > 0.0f && vy == 0.0f)
+            {
+                vx = 0.0f;
+                vy = speed;
+                CmdMove(vx, vy);
+            }
+            else if (Input.GetAxis("Vertical") < 0.0f && vy == 0.0f)
+            {
+                vx = 0.0f;
+                vy = -speed;
+                CmdMove(vx, vy);
+            }
         }
 
+        // if (NetworkServer.active)
         transform.Translate(vx * Time.deltaTime, vy * Time.deltaTime, 0.0f);
+    }
+
+    [Command]
+    public void CmdMove(float x, float y)
+    {
+        RpcMove(x, y);
+    }
+
+    [ClientRpc]
+    public void RpcMove(float x, float y)
+    {
+        vx = x;
+        vy = y;
     }
 }
